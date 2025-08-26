49bdcb043d21e8ac8c6a24732dda731b111db306
Nicholas B <brunernick11@gmail.com>
2025-08-26 11:33:21 -0500

    Changed SQLAlchemy to MySQL connector and defined show table schemas

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Overwatch MySQL Database
+This web app connects to a local database I made using MySQL WorkBench filled with data over character from the videogame Overwatch and generates tables for the user to query revelent information regardng the characters.
 
 ## Installation
 Assuming python is installed, install the following modules:
 ```
 pip install flask
-pip install flask-sqlalchemy
 pip install mysql-connector-python
-pip install pymysql
-pip install cryptography
 ```
--- flask-app/app.py
+++ flask-app/app.py
@@ -1,22 +1,55 @@
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask import render_template
 import os
-from flask_sqlalchemy import SQLAlchemy
+from mysql.connector import connect, Error
 
-user = os.getenv("MYSQL_USERNAME")
-password = os.getenv("MYSQL_PASSWORD")
-database = "Overwatch"
+load_dotenv()
+db_user = os.getenv("MYSQL_USERNAME")
+db_password = os.getenv("MYSQL_PASSWORD")
+db_port = 3306
+db_database = "Overwatch"
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{user}:{password}@localhost/{database}"
-db = SQLAlchemy(app)
+try:
+    connection = connect(
+        host="localhost",
+        user=db_user,
+        password=db_password,
+        database=db_database
+    )
+    print("Connected:", connection.is_connected())
+except Error as e:
+    print(e)
+    exit()
 
-print(db)
+def show_table(table):
+    show_table_query = f"DESCRIBE {table}"
+    with connection.cursor() as cursor:
+        cursor.execute(show_table_query)
+        # Fetch rows from last executed query
+        result = []
+        for row in cursor.fetchall():
+            result.append(str(row))
+        print(result)
+        return result
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/characters")
+def characters():
+    return render_template("table.html", table_name="characters", table=show_table("characters"))
+
+@app.route("/affiliations")
+def affiliations():
+    return render_template("table.html", table_name="affiliations", table=show_table("affiliations"))
+
+@app.route("/characteraffiliations")
+def characteraffiliations():
+    return render_template("table.html", table_name="characteraffiliations", table=show_table("characteraffiliations"))
 
 if __name__ == '__main__':
     app.run()
