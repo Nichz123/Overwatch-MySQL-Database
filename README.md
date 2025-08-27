18ee6b26ac564a5fac20b09575628e983499ed55
Nicholas B <brunernick11@gmail.com>
2025-08-27 18:14:56 -0500

    Executes SELECT SQL statements

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # MySQL Database
 This web app connects to a local database made using MySQL WorkBench filled generates tables for the user to query revelent information regardng the characters.
 
+Note that this app only allows SQL SELECT statements to avoid modifying origonal database.
+
+<img src="image.png" width="64px" alt="Example of Web App"></img>
+
+Also note that in this example I use a local database I made of characters from a videogame I play called Overwatch.
+
 ## Installation
 Assuming python is installed, install the following modules:
 ```
@@ -10,8 +16,9 @@ pip install mysql-connector-python
 After creating a MySQL Wrokbench database connect it by creating a .env with the following information:
 ```
 MYSQL_USERNAME = WOUR_USERNAME
+MYSQL_HOST = YOUR_HOST
 MYSQL_PASSWORD = YOUR_PASSWORD
 MYSQL_PORT = YOUR_PORT
 MYSQL_DATABASE = YOUR_DATABASE
 ```
-Replace YOUR_* fields with the relevant info.
+Replace YOUR_* fields with the relevant info from your MySQL Workbench
--- flask-app/app.py
+++ flask-app/app.py
@@ -6,6 +6,7 @@ from mysql.connector import connect, Error
 
 load_dotenv()
 db_user = os.getenv("MYSQL_USERNAME")
+db_host = os.getenv("MYSQL_HOST")
 db_password = os.getenv("MYSQL_PASSWORD")
 db_port = os.getenv("MYSQL_PORT")
 db_database = os.getenv("MYSQL_DATABASE")
@@ -14,7 +15,7 @@ app = Flask(__name__)
 
 try:
     connection = connect(
-        host="localhost",
+        host=db_host,
         user=db_user,
         password=db_password,
         database=db_database
@@ -42,14 +43,38 @@ def show_table(table):
             result.append(str(row))
         return result
 
+# Grab the tables and their data from the database
+tables = find_tables()
+table_datas = []
+for i in range(0, len(tables)):
+    tables[i]=str(tables[i])[2:-3]
+    table_datas.append(show_table(tables[i]))
+
+# Function to run user inputted sql code
+def execute_sql(query):
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        table = cursor.fetchall()
+        return table
+
 @app.route('/')
-def home():
-    tables = find_tables()
-    table_datas = []
-    for i in range(0, len(tables)):
-        tables[i]=str(tables[i])[2:-3]
-        table_datas.append(show_table(tables[i]))
-    return render_template('index.html', database=db_database, tables=tables, len=len(tables), table_datas=table_datas)
+def index():
+    return render_template('index.html', form_text="", database=db_database, tables=tables, len=len(tables), table_datas=table_datas)
+
+@app.route('/sql_results', methods=['POST'])
+def sql_results():
+    query = request.form.get('chat-input')
+    try:
+        result = execute_sql(query)
+    except Exception:
+        return render_template('sql_fail.html', form_text=query, database=db_database, tables=tables, len=len(tables), table_datas=table_datas)
+    else:
+        # Only SELECT statements
+        if query[0:6] != "select":
+            return render_template('sql_fail.html', form_text=query, database=db_database, tables=tables, len=len(tables), table_datas=table_datas)
+        table_height = len(result)
+        table_width = len(result[0])
+        return render_template('sql_results.html', form_text=query, width=table_width, height=table_height, result=result, database=db_database, tables=tables, len=len(tables), table_datas=table_datas)
 
 if __name__ == '__main__':
     app.run()
